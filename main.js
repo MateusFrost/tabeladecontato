c019f185460883d0073d3f81186cb264c46b1b8c
Mateus Conarinh <Mateusdesouzac@gmail.com>
2024-02-19 23:09:58 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById("form-contato").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var nome = document.getElementById("nome").value;
+    var telefone = document.getElementById("telefone").value;
+
+    var table = document.getElementById("tabela-contatos").getElementsByTagName("tbody")[0];
+    var newRow = table.insertRow(table.rows.length);
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    cell1.innerHTML = nome;
+    cell2.innerHTML = telefone;
+
+    document.getElementById("form-contato").reset();
+});
